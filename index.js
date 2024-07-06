2ce06adec7bd46d529c6a7bc3226e30a4c8b69c6
Prathamesh <prathameshkothalkar9021@gmail.com>
2024-07-06 08:56:22 +0530

    Cors

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express")
 const app=express()
 const router = require("./router");
-
+const cors = require("cors")
 
 app.use(express.json())
+app.use(cors())
 app.use("/api/v1",router);
 app.get("/",(req,res)=>{
     res.send("Hello World");
 })
 
-
-app.listen(3000,()=>{console.log("Server is running");})
+const port = process.env.PORT || 3000
+app.listen(port,()=>{console.log("Server is running ",port);})
